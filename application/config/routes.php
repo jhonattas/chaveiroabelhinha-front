6ee3a6ef344ec4366170d76eedd2be9af6932443
Jhonattas Henrique <jhonattas@soucriador.com>
2018-10-29 14:29:01 -0300

    adiciona a linha automotiva

--- application/config/routes.php
+++ application/config/routes.php
@@ -3,6 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'welcome';
 
 $route['residencial'] = 'welcome/residencial';
+$route['automotiva'] = 'welcome/automotiva';
 
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
