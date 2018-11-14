12b39bc53b6c0c904d865a368de2b5a834e60d91
Jhonattas Henrique <jhonattas@soucriador.com>
2018-11-13 23:38:12 -0200

    atualiza com um botao para o facebook

--- application/views/obrigado.php
+++ application/views/obrigado.php
@@ -206,7 +206,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 								href="https://www.google.com.br/search?ei=u3nrW6WtOoqEwgTUqZjgCQ&q=chaveiro+abelhinha+24+horas&oq=chaveiro+abelhinha+24+horas&gs_l=psy-ab.3..33i22i29i30k1.126060.130395.0.130497.27.18.0.0.0.0.306.1945.0j4j4j1.9.0....0...1c.1.64.psy-ab..18.8.1784...0j0i131k1j0i131i10k1j0i22i30k1.0.B5jdGrzDIek#lrd=0x94c8c58b9073d37d:0xf3f7bb4010900ec8,3,,,"
 								class="btn btn-danger"
 								target="_google"
-							>NOS AVALIE NO GOOGLE</a>
+							>NOS AVALIE NO GOOGLE</a>  
+
+							<a
+								href="https://facebook.com/chaveiroabelhinha/reviews"
+								class="btn btn-primary"
+								target="_facebook"
+								>
+								NOS AVALIE NO FACEBOOK
+							</a>
             </p>
             <br/>
             
