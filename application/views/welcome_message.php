89e0143ee21d2b9c6f7cfccd35d5d2b924b3e04f
Jhonattas Henrique <jhonattas@soucriador.com>
2020-07-23 17:19:03 -0300

    remove a font da google

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -8,7 +8,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="stylesheet" href="<?php echo base_url('assets/vendor/bootstrap/css/bootstrap.min.css'); ?>" />
   <link rel="stylesheet" href="<?php echo base_url('assets/css/scrolling-nav.css'); ?>" />
-  <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Open+Sans&display=swap" />
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
 
 	<link rel="apple-touch-icon" sizes="57x57" href="<?php echo base_url('assets/apple-icon-57x57.png'); ?>">
